5497a9b474fbbc7eeb8035a4e489765da6f06d66
ranjith <ranjith@ranjiths-macbook-pro.local>
2015-09-14 00:51:25 +0530

    Initial Commit

---
+++ SwitchVCUsingSegmentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwitchVCUsingSegmentControl
+//
+//  Created by ranjith on 14/09/15.
+//  Copyright © 2015 ranjith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
